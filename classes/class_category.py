202f0e7676503b8dc7261ad314392d6fe447c958
Alexei125 <160236532+Alexei125@users.noreply.github.com>
2024-04-02 19:42:06 +0300

    task2

--- classes/class_category.py
+++ classes/class_category.py
@@ -67,8 +67,7 @@ class Product:
     def __str__(self):
         return f'{self.name}, {self.__price} руб. Остаток: {self.quantity} шт.'
 
-    def __add__(self, other):
-        return self.__price * self.quantity + other.price * other.quantity
+
 
     def __add__(self, other):
         if isinstance(other, self.__class__):
